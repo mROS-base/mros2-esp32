491c03ec43cf0ec88ccc53226850669b8858f3eb
s-hosoai <shintaro.hosoai@gmail.com>
2023-04-06 15:58:19 +0900

    impl lwip wrapper

---
+++ components/mros2/embeddedRTPS/thirdparty/lwip/lwip.c
@@ -0,0 +1,30 @@
+#include "lwip.h"
+#include "lwipopts.h"
+#include "cmsis_os.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include <stdio.h>
+
+static SemaphoreHandle_t xMutex = NULL;
+
+void MX_LWIP_Init(void)
+{
+  sys_init();
+  mem_init();
+  memp_init();
+  udp_init();
+  xMutex = xSemaphoreCreateMutex();
+  return;
+}
+
+void sys_lock_tcpip_core(void)
+{
+  xSemaphoreTake(xMutex, portMAX_DELAY);
+  return;
+}
+
+void sys_unlock_tcpip_core(void)
+{
+  xSemaphoreGive(xMutex);
+  return;
+}
---
+++ components/mros2/embeddedRTPS/thirdparty/lwip/lwip.h
@@ -0,0 +1,15 @@
+#ifndef _LWIP_H_
+#define _LWIP_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  extern void MX_LWIP_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LWIP_H_ */
