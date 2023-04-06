e3697868f2ea4d119895ee94764d4ade3885e5bd
s-hosoai <shintaro.hosoai@gmail.com>
2023-04-06 15:19:56 +0900

    copy sample app, rename to cpp

---
+++ main/mros2-esp32.cpp
@@ -0,0 +1,47 @@
+// #include <stdio.h>
+// #include "mros2.h"
+
+// extern "C" void app_main(void)
+// {
+//   mros2::init(0, NULL);
+// }
+
+#include "mros2.h"
+#include "std_msgs/msg/string.hpp"
+
+#include "cmsis_os.h"
+#include "netif.h"
+
+#include <stdio.h>
+#include <string.h>
+
+mros2::Subscriber sub;
+mros2::Publisher pub;
+
+void userCallback(std_msgs::msg::String *msg)
+{
+  printf("subscribed msg: '%s'\r\n", msg->data.c_str());
+  printf("publishing msg: '%s'\r\n", msg->data.c_str());
+  pub.publish(*msg);
+}
+
+extern "C" void app_main(void)
+{
+  // maybe need to initialize wifi
+  // netif_posix_add(NETIF_IPADDR, NETIF_NETMASK);
+
+  osKernelStart();
+
+  printf("mros2-posix start!\r\n");
+  printf("app name: echoreply_string\r\n");
+  mros2::init(0, NULL);
+  MROS2_DEBUG("mROS 2 initialization is completed\r\n");
+
+  mros2::Node node = mros2::Node::create_node("mros2_node");
+  pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
+  sub = node.create_subscription<std_msgs::msg::String>("to_stm", 10, userCallback);
+  vTaskDelay(100);
+  MROS2_INFO("ready to pub/sub message\r\n");
+
+  mros2::spin();
+}
