07ea05608661aff479de3a407913c053d50b6b46
s-hosoai <shintaro.hosoai@gmail.com>
2023-05-11 17:31:34 +0900

    rename and restruct main app

--- main/app.cpp
+++ main/app.cpp
@@ -10,11 +10,11 @@
 #include "std_msgs/msg/string.hpp"
 
 #include "cmsis_os.h"
-#include "netif.h"
 #include "wifi.h"
 
 #include <stdio.h>
 #include <string.h>
+// #include "M5Unified.hpp"
 
 mros2::Subscriber sub;
 mros2::Publisher pub;
@@ -33,18 +33,33 @@ extern "C" void app_main(void)
   init_wifi();
 
   osKernelStart();
+  // auto cfg = M5.config();
+  // M5.begin(cfg);
+  // M5.Lcd.setTextSize(2);
+  // M5.Lcd.println("Press BtnA");
 
-  printf("mros2-posix start!\r\n");
-  printf("app name: echoreply_string\r\n");
+  printf("mbed mros2 start!\r\n");
+  printf("app name: echoback_string\r\n");
   mros2::init(0, NULL);
   MROS2_DEBUG("mROS 2 initialization is completed\r\n");
 
   mros2::Node node = mros2::Node::create_node("mros2_node");
-  pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
-  sub = node.create_subscription<std_msgs::msg::String>("to_stm", 10, userCallback);
-  vTaskDelay(100);
+  mros2::Publisher pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
+  // mros2::Subscriber sub = node.create_subscription<std_msgs::msg::String>("to_stm", 10, userCallback);
+
+  osDelay(100);
   MROS2_INFO("ready to pub/sub message\r\n");
 
-  mros2::spin();
-}
+  auto count = 0;
+  while (1)
+  {
+    auto msg = std_msgs::msg::String();
+    msg.data = "Hello from mros2-mbed : " + std::to_string(count++);
+    printf("publishing msg: '%s'\r\n", msg.data.c_str());
+    pub.publish(msg);
+    osDelay(1000);
+  }
 
+  mros2::spin();
+  return;
+}
