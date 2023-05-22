338fc01ed906f0bf12c273942fdf461c39120eb0
s-hosoai <shintaro.hosoai@gmail.com>
2023-05-22 18:46:38 +0900

    fix wifi info

--- workspace/common/wifi/wifi.h
+++ workspace/common/wifi/wifi.h
@@ -1,7 +1,7 @@
 #include "esp_wifi_types.h"
 
-#define EXAMPLE_ESP_WIFI_SSID "quantumnet-g"
-#define EXAMPLE_ESP_WIFI_PASS "55ccfd431ac9a"
+#define EXAMPLE_ESP_WIFI_SSID "SSID"
+#define EXAMPLE_ESP_WIFI_PASS "PASSWORD"
 #define EXAMPLE_ESP_MAXIMUM_RETRY 3
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
 
