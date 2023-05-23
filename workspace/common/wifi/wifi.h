895501cf0612562c04bdf4ba3743c28a571602a9
s-hosoai <shintaro.hosoai@gmail.com>
2023-05-23 11:39:59 +0900

    revert wifi.h

---
+++ workspace/common/wifi/wifi.h
@@ -0,0 +1,23 @@
+#include "esp_wifi_types.h"
+
+#define EXAMPLE_ESP_WIFI_SSID "SSID"
+#define EXAMPLE_ESP_WIFI_PASS "PASS"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 3
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+// #define STATIC_IP
+#ifdef STATIC_IP
+#define NETIF_IPADDR "192.168.11.107"
+#define NETIF_NETMASK "255.255.255.0"
+#define NETIF_GW "192.168.11.1"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void init_wifi(void);
+#ifdef __cplusplus
+}
+#endif
