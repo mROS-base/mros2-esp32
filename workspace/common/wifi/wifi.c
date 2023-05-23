907428fb464b0563b4fc87aec1f3c46dd8ff87fc
s-hosoai <shintaro.hosoai@gmail.com>
2023-05-23 13:56:12 +0900

    remove EXAMPLE_ prefix

--- workspace/common/wifi/wifi.c
+++ workspace/common/wifi/wifi.c
@@ -45,7 +45,7 @@ static void event_handler(void *arg, esp_event_base_t event_base,
 #endif
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        if (s_retry_num < ESP_MAXIMUM_RETRY)
         {
             esp_wifi_connect();
             s_retry_num++;
@@ -93,8 +93,8 @@ void wifi_init_sta(void)
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
+            .ssid = ESP_WIFI_SSID,
+            .password = ESP_WIFI_PASS,
             .threshold = {.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD},
             .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
         },
@@ -118,12 +118,12 @@ void wifi_init_sta(void)
     if (bits & WIFI_CONNECTED_BIT)
     {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
     }
     else if (bits & WIFI_FAIL_BIT)
     {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
     }
     else
     {
