70a46ca390e86c06d336207aed5c0b608b0b7662
Ar-Ray-code <ray255ar@gmail.com>
2023-06-09 00:15:20 +0900

    add dev-tool

---
+++ dev_tool/README.md
@@ -0,0 +1,29 @@
+# mros2-esp32 dev-tool
+
+Building mros2-esp32 tool using docker.
+
+<br>
+
+## Requirements
+
+- USB accessable docker-compose environment
+
+<br>
+
+## Usage
+
+M5Stack CoreS3
+
+```bash
+bash build_example.bash /dev/ttyACM0 ./mros2-esp32 ../workspace/echoback_string esp32s3
+```
+
+### Arguments
+
+- 1: Serial port (default: `/dev/ttyUSB0`)
+- 2: Path to mros2-esp32 (default: `./mros2-esp32`)
+    - If this directory does not exist, it will be cloned from [mROS-base/mros2-esp32](https://github.com/mROS-base/mros2-esp32).
+- 3: Path to workspace (default: `../workspace/echoback_string`)
+- 4: Target board (esp32, esp32s2, esp32s3...) (default: `esp32`)
+
+<br>
---
+++ dev_tool/build_example.bash
@@ -0,0 +1,28 @@
+# if mros2-esp32 not found : clone https://github.com/mROS-base/mros2-esp32.git --recursive
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PORT=${1:-/dev/ttyUSB0}
+MROS2_DIR=${2:-${SCRIPT_DIR}/mros2-esp32}
+WORKSPACE=${3:-${SCRIPT_DIR}/../workspace/echoback_string}
+TARGET=${4:-esp32}
+
+echo "====================="
+echo "PORT: ${PORT}"
+echo "MROS2_DIR: ${MROS2_DIR}"
+echo "WORKSPACE: ${WORKSPACE}"
+echo "TARGET: ${TARGET}"
+echo "====================="
+
+rm -rf ${SCRIPT_DIR}/.env
+echo "PORT=${PORT}" > ${SCRIPT_DIR}/.env
+echo "MROS2_DIR=${MROS2_DIR}" >> ${SCRIPT_DIR}/.env
+echo "WORKSPACE=${WORKSPACE}" >> ${SCRIPT_DIR}/.env
+echo "TARGET=${TARGET}" >> ${SCRIPT_DIR}/.env
+
+
+cd ${SCRIPT_DIR}
+
+if [ ! -d ${SCRIPT_DIR}/mros2-esp32 ]; then
+    git clone --recursive https://github.com/mROS-base/mros2-esp32.git -b main
+fi
+
+docker-compose up --build mros2_esp32
